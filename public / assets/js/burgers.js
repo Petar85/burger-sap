aff492ca72e2364855babd94ac05b1a3c6d3b64c
Peter Pan <petarzivkovic85@gmail.com>
2020-10-15 01:19:37 -0700

    created and initialized database

--- public / assets/js/burgers.js
+++ public / assets/js/burgers.js
@@ -1,20 +1,23 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
-    $(".change-sleep").on("click", function(event) {
-      var id = $(this).data("id");
-      var newSleep = $(this).data("newsleep");
+    $(".create-form").on("submit", function(event) {
+      event.preventDefault();
+      
   
-      var newSleepState = {
-        sleepy: newSleep
+      var newBurger= {
+        burger_name: $("#newburger")
+          .val()
+          .trim(),
+          devoured: 0
       };
   
       // Send the PUT request.
-      $.ajax("/api/cats/" + id, {
+      $.ajax("/api/burgers/" + id, {
         type: "PUT",
-        data: newSleepState
+        data: newBurger
       }).then(
         function() {
-          console.log("changed sleep to", newSleep);
+          console.log("added new burger");
           // Reload the page to get the updated list
           location.reload();
         }
